96a5d0f00016f4d9b0396681fe7774833eb9ec83
Pedro H Prado <pedrohenriquepradodenovaes@gmail.com>
2025-08-22 22:09:34 -0300

    Add files via upload

---
+++ DesenvolventoWebIII/Aula03/src/routes/clienteRoutes.ts
@@ -0,0 +1,38 @@
+import { Router, Request, Response} from "express";
+import Cliente from "../models/Cliente"
+
+const router = Router()
+
+// Rota POST -> Criar novo cliente
+
+router.post('/', async (req: Request, res: Response) => {
+    try{
+        const novoCliente = new Cliente(req.body);
+        const clienteSalvo = await novoCliente.save(); //salva no MongoDB
+        res.status(201).json(clienteSalvo);
+    }catch(erro: unknown){
+        // Tratamento seguro do erro (unknow)
+        if (erro instanceof Error){
+            res.status(400).json({erro: erro.message});
+        } else {
+            res.status(400).json({ erro: String(erro) });
+        }
+    }
+});
+
+//Rot GET -> Listar os clientes cadastrados
+
+router.get("/", async (_req: Request, res: Response) => {
+    try{
+        const clientes = await Cliente.find(); //buscar todos os clientes
+        res.json(clientes);
+    } catch (erro: unknown) {
+        if (erro instanceof Error){
+            res.status(500).json({ erro: erro.message });
+        } else {
+            res.status(500).json ({ erro: String(erro) });
+        }
+    }
+});
+
+export default router;
